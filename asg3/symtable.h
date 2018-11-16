57f9d363db0b0c55f3c24cabfad27ad8a394f06c
Vineet Ramareddi <vramared@soe.ucsc.edu>
2018-11-16 12:28:59 -0800

    made sym header and cpp files

---
+++ asg3/symtable.h
@@ -0,0 +1,35 @@
+#ifndef __SYMTABLE_H__
+#define __SYMTABLE_H__
+
+#include <bitset>
+#include <unordered_map>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+#include "lyutils.h"
+#include "auxlib.h"
+#include "astree.h"
+
+enum class attr: unsigned {
+       VOID, INT, NULLX, STRING, STRUCT, ARRAY, FUNCTION, VARIABLE,
+       FIELD, TYPEID, PARAM, LVAL, CONST, VREG, VADDR, BITSET_SIZE,
+};
+using attr_bitset = bitset<unsigned(attr::BITSET_SIZE)>;
+
+struct symbol {
+   attr_bitset attributes;
+   size_t sequence;
+   using symbol_table = unordered_map<string*, symbol*>;
+   symbol_table* fields;
+   location lloc;
+   size_t block_nr;
+   vector<symbol*>* parameters;
+};
+
+using symbol_table = unordered_map<string*, symbol*>;
+using symbol_entry = pair<const string*, symbol*>;
+
+
+#endif
